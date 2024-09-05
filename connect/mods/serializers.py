8bd2d4f2a53f041c3d10f77617ae5d6343afb529
Franky Gudino <frankyjg96@gmail.com>
2024-09-04 19:09:08 -0700

    remove DRF project

--- connect/mods/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from mods.models import Mod
-
-class ModSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Mod
-        fields = '__all__'
--- connect/mods/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, URLPattern
-from .views import ModView
-
-urlpatterns: URLPattern = [
-    path('', ModView.as_view()),
-]
