e218153aeac4b862bf4866303db6f3b9b376aa66
Franky Gudino <frankyjg96@gmail.com>
2024-10-21 17:42:07 -0700

    simple UI when a user is not logged in

--- nexus-connect/src/layout.tsx
+++ nexus-connect/src/layout.tsx
@@ -4,20 +4,22 @@ import { useSidebar } from './states/sidebarState';
 import AppHeader from './components/layouts/app-header';
 import { Toaster } from './components/ui/sonner';
 import SimpleBar from 'simplebar-react';
+import { useUserState } from './states/userState';
 
 export default function Layout() {
   const expanded = useSidebar((state) => state.expanded);
+  const user = useUserState((state) => state.user);
   const headerHeightPx = 60;
 
   return (
     <>
-      <AppSidebar headerHeight={headerHeightPx} />
+      {user && <AppSidebar headerHeight={headerHeightPx} />}
       <AppHeader height={headerHeightPx} />
       <SimpleBar
         style={{ height: `calc(100vh - ${headerHeightPx}px)` }}
         autoHide={false}
         className={`p-5 overflow-auto transition-all ${
-          expanded ? 'ml-[160px]' : 'ml-[52px]'
+          user ? (expanded ? 'ml-[160px]' : 'ml-[52px]') : 'ml-0'
         }`}
       >
         <Outlet />
--- nexus-connect/src/pages/home.tsx
+++ nexus-connect/src/pages/home.tsx
@@ -1,13 +1,16 @@
 import CenteredContent from '@/components/layouts/centered-content';
 import UserPage from '@/components/layouts/user-page';
+import { Button } from '@/components/ui/button';
 import useProfileApi from '@/hooks/useProfileApi';
 import { useUserState } from '@/states/userState';
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
   const user = useUserState((state) => state.user);
   const jwt = useUserState((state) => state.userJwt);
   const { getMyProfiles, profiles } = useProfileApi();
+  const navigate = useNavigate();
 
   useEffect(() => {
     getMyProfiles();
@@ -19,7 +22,17 @@ export default function Home() {
         <UserPage user={user} profiles={profiles} isMe />
       ) : (
         <CenteredContent>
-          <div>TODO: login or sign up button</div>
+          <h1 className="text-3xl">Welcome to Nexus Connect!</h1>
+          <p className="opacity-75">
+            Login or register to start sharing your mod lists with others!
+          </p>
+          <Button
+            className="mt-10"
+            size="lg"
+            onClick={() => navigate('/login')}
+          >
+            Log in / Register
+          </Button>
         </CenteredContent>
       )}
     </>
